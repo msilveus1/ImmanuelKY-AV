967b3018ddf3a77e27ed7d1acecfd844499059cb
msilveus1 <msilveus@wisc.edu>
2023-03-20 18:29:10 -0400

    feat: adding skeleton of work needed to be done for this script

---
+++ youtube_analytics/find_geographies.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import os
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
+
+API_SERVICE_NAME = 'youtubeAnalytics'
+API_VERSION = 'v2'
+CLIENT_SECRETS_FILE = 'YOUR_CLIENT_SECRET_FILE.json
